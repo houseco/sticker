5734d9032acd830ab241e01bb49191de71a2451e
phouse512 <philiphouse2015@u.northwestern.edu>
2018-02-21 21:28:18 -0600

    setup fake working handler

--- server/handler.js
+++ server/handler.js
@@ -10,12 +10,27 @@ const {
 
 const getGreeting = firstName => `Hello, ${firstName}.`;
 
-module.exports.query = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-};
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      greeting: {
+        args: { 
+          firstName: { 
+            name: 'firstName', 
+            type: new GraphQLNonNull(GraphQLString),
+          },
+        },
+        type: GraphQLString,
+        resolve: (parent, args) => getGreeting(args.firstName),
+      },
+    },
+  }),
+});
+
+module.exports.query = (event, context, callback) => graphql(schema, event.queryStringParameters.query)
+  .then(
+    result => callback(null, {statusCode: 200, body: JSON.stringify(result)}),
+    err => callback(err)
+  )
+
